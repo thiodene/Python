9e961e2721af30a0f6e4677f6e9f298390e5d311
Serge Ayissi <ayissi.serge@gmail.com>
2019-04-22 09:37:50 -0400

    Create config_aqi.py

---
+++ class/aqi/config_aqi.py
@@ -0,0 +1,176 @@
+############################################## AQI CONFIG ###########################################
+
+############################# CO Gas
+# Good:
+co_min_good_ppm = 0                                      # Don't change the Zero value!
+co_max_good_ppm = 0
+
+# Moderate:
+co_min_moderate_ppm = co_max_good_ppm                    # Don't change this value!
+co_max_moderate_ppm = 0
+
+# Sensitive:
+co_min_sensitive_ppm = co_max_moderate_ppm               # Don't change this value!
+co_max_sensitive_ppm = 0
+
+# Unhealthy:
+co_min_unhealthy_ppm = co_max_sensitive_ppm              # Don't change this value!
+co_max_unhealthy_ppm = 0
+
+# Very Unhealthy:
+co_min_very_ppm = co_max_unhealthy_ppm                   # Don't change this value!
+co_max_very_ppm = 0
+
+# Hazardous:
+co_min_hazardous_ppm = co_max_very_ppm                   # Don't Change this value!
+co_max_hazardous_ppm = 0
+
+############################# NO2 Gas
+# Good:
+no2_min_good_ppm = 0                                     # Don't change the Zero value!
+no2_max_good_ppm = 0
+
+# Moderate:
+no2_min_moderate_ppm = no2_max_good_ppm                  # Don't change this value!
+no2_max_moderate_ppm = 0
+
+# Sensitive:
+no2_min_sensitive_ppm = no2_max_moderate_ppm             # Don't change this value!
+no2_max_sensitive_ppm = 0
+
+# Unhealthy:
+no2_min_unhealthy_ppm = no2_max_sensitive_ppm            # Don't change this value!
+no2_max_unhealthy_ppm = 0
+
+# Very Unhealthy:
+no2_min_very_ppm = no2_max_unhealthy_ppm                 # Don't change this value!
+no2_max_very_ppm = 0
+
+# Hazardous:
+no2_min_hazardous_ppm = no2_max_very_ppm                 # Don't change this value!
+no2_max_hazardous_ppm = 0
+
+############################# O3 Gas
+# Good:
+o3_min_good_ppm = 0                                     # Don't change the Zero value!
+o3_max_good_ppm = 2
+
+# Moderate:
+o3_min_moderate_ppm = o3_max_good_ppm                  # Don't change this value!
+o3_max_moderate_ppm = 4
+
+# Sensitive:
+o3_min_sensitive_ppm = o3_max_moderate_ppm             # Don't change this value!
+o3_max_sensitive_ppm = 8
+
+# Unhealthy:
+o3_min_unhealthy_ppm = o3_max_sensitive_ppm            # Don't change this value!
+o3_max_unhealthy_ppm = 16
+
+# Very Unhealthy:
+o3_min_very_ppm = o3_max_unhealthy_ppm                 # Don't change this value!
+o3_max_very_ppm = 32
+
+# Hazardous:
+o3_min_hazardous_ppm = o3_max_very_ppm                 # Don't change this value!
+o3_max_hazardous_ppm = 64
+
+############################# PM1
+# Good:
+pm1_min_good_ugm3 = 0                                     # Don't change the Zero value!
+pm1_max_good_ugm3 = 0
+
+# Moderate:
+pm1_min_moderate_ugm3 = pm1_max_good_ugm3                  # Don't change this value!
+pm1_max_moderate_ugm3 = 0
+
+# Sensitive:
+pm1_min_sensitive_ugm3 = pm1_max_moderate_ugm3             # Don't change this value!
+pm1_max_sensitive_ugm3 = 0
+
+# Unhealthy:
+pm1_min_unhealthy_ugm3 = pm1_max_sensitive_ugm3            # Don't change this value!
+pm1_max_unhealthy_ugm3 = 0
+
+# Very Unhealthy:
+pm1_min_very_ugm3 = pm1_max_unhealthy_ugm3                 # Don't change this value!
+pm1_max_very_ugm3 = 0
+
+# Hazardous:
+pm1_min_hazardous_ugm3 = pm1_max_very_ugm3                 # Don't change this value!
+pm1_max_hazardous_ugm3 = 0
+
+############################# PM2.5
+# Good:
+pm25_min_good_ugm3 = 0                                     # Don't change the Zero value!
+pm25_max_good_ugm3 = 0
+
+# Moderate:
+pm25_min_moderate_ugm3 = pm25_max_good_ugm3                  # Don't change this value!
+pm25_max_moderate_ugm3 = 0
+
+# Sensitive:
+pm25_min_sensitive_ugm3 = pm25_max_moderate_ugm3             # Don't change this value!
+pm25_max_sensitive_ugm3 = 0
+
+# Unhealthy:
+pm25_min_unhealthy_ugm3 = pm25_max_sensitive_ugm3            # Don't change this value!
+pm25_max_unhealthy_ugm3 = 0
+
+# Very Unhealthy:
+pm25_min_very_ugm3 = pm25_max_unhealthy_ugm3                 # Don't change this value!
+pm25_max_very_ugm3 = 0
+
+# Hazardous:
+pm25_min_hazardous_ugm3 = pm25_max_very_ugm3                 # Don't change this value!
+pm25_max_hazardous_ugm3 = 0
+
+############################# PM4
+# Good:
+pm4_min_good_ugm3 = 0                                     # Don't change the Zero value!
+pm4_max_good_ugm3 = 0
+
+# Moderate:
+pm4_min_moderate_ugm3 = pm4_max_good_ugm3                  # Don't change this value!
+pm4_max_moderate_ugm3 = 0
+
+# Sensitive:
+pm4_min_sensitive_ugm3 = pm4_max_moderate_ugm3             # Don't change this value!
+pm4_max_sensitive_ugm3 = 0
+
+# Unhealthy:
+pm4_min_unhealthy_ugm3 = pm4_max_sensitive_ugm3            # Don't change this value!
+pm4_max_unhealthy_ugm3 = 0
+
+# Very Unhealthy:
+pm4_min_very_ugm3 = pm4_max_unhealthy_ugm3                 # Don't change this value!
+pm4_max_very_ugm3 = 0
+
+# Hazardous:
+pm4_min_hazardous_ugm3 = pm4_max_very_ugm3                 # Don't change this value!
+pm4_max_hazardous_ugm3 = 0
+
+############################# PM10
+# Good:
+pm10_min_good_ugm3 = 0                                     # Don't change the Zero value!
+pm10_max_good_ugm3 = 0
+
+# Moderate:
+pm10_min_moderate_ugm3 = pm10_max_good_ugm3                  # Don't change this value!
+pm10_max_moderate_ugm3 = 0
+
+# Sensitive:
+pm10_min_sensitive_ugm3 = pm10_max_moderate_ugm3             # Don't change this value!
+pm10_max_sensitive_ugm3 = 0
+
+# Unhealthy:
+pm10_min_unhealthy_ugm3 = pm10_max_sensitive_ugm3            # Don't change this value!
+pm10_max_unhealthy_ugm3 = 0
+
+# Very Unhealthy:
+pm10_min_very_ugm3 = pm10_max_unhealthy_ugm3                 # Don't change this value!
+pm10_max_very_ugm3 = 0
+
+# Hazardous:
+pm10_min_hazardous_ugm3 = pm10_max_very_ugm3                 # Don't change this value!
+pm10_max_hazardous_ugm3 = 0
